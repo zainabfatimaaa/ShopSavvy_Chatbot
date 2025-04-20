e7b9c6142d590778a321e74498ec687d2ef40537
zainabfatimaaa <25100208@lums.edu.pk>
2025-04-20 23:12:35 +0500

    s

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Zainab\work\Senior_Fall\LLMS\project\ChatBot\backend\venv
+set VIRTUAL_ENV=C:\Users\ChatBotBackend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
